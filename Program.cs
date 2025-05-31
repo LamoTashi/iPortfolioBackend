fce6eef7a473fda5a98d61cbd8f9e36ca22762a4
Tashi Lamo <tashilamo2005@gmail.com>
2025-05-31 13:51:05 +0530

    reset DB

--- Program.cs
+++ Program.cs
@@ -48,14 +48,14 @@ string ConvertDatabaseUrlToConnectionString(string databaseUrl)
     var builder = new NpgsqlConnectionStringBuilder
     {
         Host = uri.Host,
-        Port = uri.Port,
+        Port = uri.IsDefaultPort ? 5432 : uri.Port, // Safe fallback to default port
         Username = userInfo[0],
-        Password = userInfo[1],
+        Password = userInfo.Length > 1 ? userInfo[1] : "",
         Database = uri.AbsolutePath.TrimStart('/'),
         SslMode = SslMode.Require,
-        TrustServerCertificate = true,
         Pooling = true
     };
+
     return builder.ConnectionString;
 }
 
